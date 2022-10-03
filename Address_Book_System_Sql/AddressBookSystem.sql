46a3257251af98fc0e8c7bd1c0e4b09c6678dedd
Sandeep singh <sandy180388@gmail.com>
2022-10-03 19:05:56 +0530

    [sandeep] added Creat address book table file to UC_2

--- Address_Book_System_Sql/AddressBookSystem.sql
+++ Address_Book_System_Sql/AddressBookSystem.sql
@@ -1,3 +1,15 @@
 ﻿-----UC-1 Create Address Book Service Database-----
 create database Address_Book_Services
 use Address_Book_Service
+-----UC-2 Ability to create address book table-----
+create table address_book_table
+(
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+ZipCode varchar(255),
+PhoneNumber bigint,
+email varchar(25)
+) 
