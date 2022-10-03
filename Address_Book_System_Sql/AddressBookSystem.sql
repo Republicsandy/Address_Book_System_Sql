e4f1a4da0de135c585ef855e4c2e1421982404b7
Sandeep singh <sandy180388@gmail.com>
2022-10-03 19:02:29 +0530

    [sandeep] added Creat address Book services db file to UC_1

---
+++ Address_Book_System_Sql/AddressBookSystem.sql
@@ -0,0 +1,3 @@
+﻿-----UC-1 Create Address Book Service Database-----
+create database Address_Book_Services
+use Address_Book_Service
---
+++ Address_Book_System_Sql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_System_Sql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Address Book System");
+        }
+    }
+}
