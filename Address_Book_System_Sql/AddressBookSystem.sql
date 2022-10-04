f4acf6da91b7dc648bce650c7dc5b817aad0d838
Sandeep singh <sandy180388@gmail.com>
2022-10-04 06:55:01 +0530

    [sandeep] added ability to edit existing contact file to UC_4

--- Address_Book_System_Sql/AddressBookSystem.sql
+++ Address_Book_System_Sql/AddressBookSystem.sql
@@ -17,3 +17,7 @@ email varchar(25)
 -----UC-3 Ability to insert values to address book table-----
 insert into address_book_table(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,email)values('Ram','Kumar','Egmore','Chennai','TamilNadu','600078','9841578941','ram@gmail.com'),('Uma','Devi','Menod','BPmark','UP','ZIP8531','7841526987','umas@gmail.com'),('Amir','Khan','Ambattur','Chennai','Tamil Nadu','600660','7744556699','amir@gmail.com'),('Dhanush','Raj','XXXX','yyyy','Banglore','600XXX','4477885599','raj@gmail.com')
 select * from address_book_table
+-----UC-4 Ability to edit based on name-----
+update address_book_table set PhoneNumber = '7812453679' where FirstName = 'Uma'
+update address_book_table set PhoneNumber = '7745124574',Address = 'Mall Road' where FirstName = 'Dhanush'
+update address_book_table set email ='amir123@gmail.com' where FirstName = 'Amir'
